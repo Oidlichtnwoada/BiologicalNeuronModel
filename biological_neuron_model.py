815cef7c06d8c8f5c421d0f22c44182880471b05
Hannes Brantner <hannes.brantner@hotmail.com>
2020-12-21 09:50:48 +0100

    working memory cell with sparse inputs

--- biological_neuron_model.py
+++ biological_neuron_model.py
@@ -8,7 +8,9 @@ import pandas as pd
 
 # here we choose the initial values for the trainable parameters
 def values_for_initialization(init_id):
-    if init_id == 1:
+    if init_id == 0:
+        init_param = [64, 64, 1.1, 1.1, 8, 8]
+    elif init_id == 1:
         init_param = np.random.rand(6, 1)
     elif init_id == 2:
         init_param = 200 * np.random.rand(6, 1)
@@ -24,14 +26,14 @@ def sigmoid(x, sigma, mu):
 
 # function returns the learned parameters and the history of the two potentials of the neurons of the last training epoch
 def train(initial_values):
-    training_epochs = 10
+    training_epochs = 1
     simulation_time = 2499
-    learning_rate = 0.01
+    learning_rate = 0
 
     # constant parameters of the network
     # neuron parameters
     c = 50
-    gL = +1
+    gL = 0.4
     eL = -70
     # synaptic parameters
     eX = 0
@@ -40,19 +42,12 @@ def train(initial_values):
     m = -40
 
     # training variables
-    g_input2x = np.zeros([simulation_time, 1])  # g_input2x = conductance of the synaptic connection between input and neuron x
-    g_input2y = np.zeros([simulation_time, 1])
-    g_x2x = np.zeros([simulation_time, 1])
-    g_y2y = np.zeros([simulation_time, 1])
-    g_y2x = np.zeros([simulation_time, 1])
-    g_x2y = np.zeros([simulation_time, 1])
-
-    g_input2x[0:simulation_time - 1] = initial_values[0]
-    g_input2y[0:simulation_time - 1] = initial_values[1]
-    g_x2x[0:simulation_time - 1] = initial_values[2]
-    g_y2y[0:simulation_time - 1] = initial_values[3]
-    g_y2x[0:simulation_time - 1] = initial_values[4]
-    g_x2y[0:simulation_time - 1] = initial_values[5]
+    g_input2x = initial_values[0]
+    g_input2y = initial_values[1]
+    g_x2x = initial_values[2]
+    g_y2y = initial_values[3]
+    g_y2x = initial_values[4]
+    g_x2y = initial_values[5]
 
     # resting membrane potential of x and y
     neuron_x = np.zeros([simulation_time, 1])
@@ -66,49 +61,51 @@ def train(initial_values):
     y_train = np.array(data.iloc[:, 2:4])
 
     # training loop
-    for epoch in range(training_epochs):
-        # update the parameters for the each training iteration
-        g_input2x[0:simulation_time - 1] = g_input2x[simulation_time - 1]
-        g_input2y[0:simulation_time - 1] = g_input2y[simulation_time - 1]
-        g_x2x[0:simulation_time - 1] = g_x2x[simulation_time - 1]
-        g_y2y[0:simulation_time - 1] = g_y2y[simulation_time - 1]
-        g_y2x[0:simulation_time - 1] = g_y2x[simulation_time - 1]
-        g_x2y[0:simulation_time - 1] = g_x2y[simulation_time - 1]
+    for _ in range(training_epochs):
 
         # simulation loop
         for t in range(simulation_time - 1):
             # the two neurons x and y are simulated
-            neuron_x[t + 1] = neuron_x[t] + (gL * (eL - neuron_x[t]) + g_input2x[t] * sigmoid(x_train[t, 0], s, m) * (eX - neuron_x[t]) + g_x2x[t] * sigmoid(neuron_x[t], s, m) * (eX - neuron_x[t]) + g_y2x[t] * sigmoid(neuron_y[t], s, m) * (eI - neuron_x[t])) / c
-            neuron_y[t + 1] = neuron_y[t] + (gL * (eL - neuron_y[t]) + g_input2y[t] * sigmoid(x_train[t, 1], s, m) * (eX - neuron_y[t]) + g_y2y[t] * sigmoid(neuron_y[t], s, m) * (eX - neuron_y[t]) + g_x2y[t] * sigmoid(neuron_x[t], s, m) * (eI - neuron_y[t])) / c
+            neuron_x[t + 1] = neuron_x[t] + (
+                    gL * (eL - neuron_x[t]) + g_input2x * sigmoid(x_train[t, 0], s, m) * (eX - neuron_x[t]) + g_x2x * sigmoid(neuron_x[t], s, m) * (eX - neuron_x[t]) + g_y2x * sigmoid(neuron_y[t],
+                                                                                                                                                                                        s, m) * (
+                            eI - neuron_x[t])) / c
+            neuron_y[t + 1] = neuron_y[t] + (
+                    gL * (eL - neuron_y[t]) + g_input2y * sigmoid(x_train[t, 1], s, m) * (eX - neuron_y[t]) + g_y2y * sigmoid(neuron_y[t], s, m) * (eX - neuron_y[t]) + g_x2y * sigmoid(neuron_x[t],
+                                                                                                                                                                                        s, m) * (
+                            eI - neuron_y[t])) / c
 
             # training parameters are getting tuned by gradient decent
-            g_input2x[t + 1] = g_input2x[t] - learning_rate * (neuron_x[t + 1] - y_train[t + 1, 0]) * sigmoid(x_train[t, 0], s, m) * (eX - neuron_x[t]) / c
-            g_input2y[t + 1] = g_input2y[t] - learning_rate * (neuron_y[t + 1] - y_train[t + 1, 1]) * sigmoid(x_train[t, 1], s, m) * (eX - neuron_y[t]) / c
-            g_x2x[t + 1] = g_x2x[t] - learning_rate * (neuron_x[t + 1] - y_train[t + 1, 0]) * sigmoid(neuron_x[t], s, m) * (eX - neuron_x[t]) / c
-            g_y2y[t + 1] = g_y2y[t] - learning_rate * (neuron_y[t + 1] - y_train[t + 1, 1]) * sigmoid(neuron_y[t], s, m) * (eX - neuron_y[t]) / c
-            g_y2x[t + 1] = g_y2x[t] - learning_rate * (neuron_x[t + 1] - y_train[t + 1, 0]) * sigmoid(neuron_y[t], s, m) * (eI - neuron_x[t]) / c
-            g_x2y[t + 1] = g_x2y[t] - learning_rate * (neuron_y[t + 1] - y_train[t + 1, 1]) * sigmoid(neuron_x[t], s, m) * (eI - neuron_y[t]) / c
+            g_input2x -= learning_rate * (neuron_x[t + 1] - y_train[t + 1, 0]) * sigmoid(x_train[t, 0], s, m) * (eX - neuron_x[t]) / c
+            g_input2y -= learning_rate * (neuron_y[t + 1] - y_train[t + 1, 1]) * sigmoid(x_train[t, 1], s, m) * (eX - neuron_y[t]) / c
+            g_x2x -= learning_rate * (neuron_x[t + 1] - y_train[t + 1, 0]) * sigmoid(neuron_x[t], s, m) * (eX - neuron_x[t]) / c
+            g_y2y -= learning_rate * (neuron_y[t + 1] - y_train[t + 1, 1]) * sigmoid(neuron_y[t], s, m) * (eX - neuron_y[t]) / c
+            g_y2x -= learning_rate * (neuron_x[t + 1] - y_train[t + 1, 0]) * sigmoid(neuron_y[t], s, m) * (eI - neuron_x[t]) / c
+            g_x2y -= learning_rate * (neuron_y[t + 1] - y_train[t + 1, 1]) * sigmoid(neuron_x[t], s, m) * (eI - neuron_y[t]) / c
 
             # set all negative parameter values to zero
-            g_input2x[t + 1] = max(g_input2x[t + 1], 0)
-            g_input2y[t + 1] = max(g_input2y[t + 1], 0)
-            g_x2x[t + 1] = max(g_x2x[t + 1], 0)
-            g_y2x[t + 1] = max(g_y2x[t + 1], 0)
-            g_x2y[t + 1] = max(g_x2y[t + 1], 0)
-            g_y2y[t + 1] = max(g_y2y[t + 1], 0)
+            g_input2x = max(g_input2x, 0)
+            g_input2y = max(g_input2y, 0)
+            g_x2x = max(g_x2x, 0)
+            g_y2x = max(g_y2x, 0)
+            g_x2y = max(g_x2y, 0)
+            g_y2y = max(g_y2y, 0)
 
-        print(g_input2x[simulation_time - 1], g_input2y[simulation_time - 1], g_x2x[simulation_time - 1], g_y2y[simulation_time - 1], g_y2x[simulation_time - 1], g_x2y[simulation_time - 1])
+        print(g_input2x, g_input2y, g_x2x, g_y2y, g_y2x, g_x2y)
 
-    return g_input2x[simulation_time - 1], g_input2y[simulation_time - 1], g_x2x[simulation_time - 1], g_y2y[simulation_time - 1], g_y2x[simulation_time - 1], g_x2y[simulation_time - 1], neuron_y, neuron_x
+    return g_input2x, g_input2y, g_x2x, g_y2y, g_y2x, g_x2y, neuron_y, neuron_x, x_train[:, 0], x_train[:, 1]
 
 
 # train the network
-init_values = values_for_initialization(2)
-aa, bb, cc, dd, ee, ff, neuron_yy, neuron_xx = train(init_values)
+init_values = values_for_initialization(0)
+aa, bb, cc, dd, ee, ff, neuron_yy, neuron_xx, x_inputs, y_inputs = train(init_values)
 
 # plot the history of both neuron potentials
 plt.figure()
-plt.plot(neuron_yy, 'b', label='neuron_y')
-plt.plot(neuron_xx, 'r', label='neuron_x')
+plt.plot(neuron_xx, 'red', label='neuron_x_outputs')
+plt.plot(x_inputs, 'green', label='neuron_x_inputs')
+plt.plot(neuron_yy, 'blue', label='neuron_y_outputs')
+plt.plot(y_inputs, 'grey', label='neuron_y_inputs')
+
 plt.legend(loc='upper right')
 plt.show()
